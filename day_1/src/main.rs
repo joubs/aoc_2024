66408167f19d071e4399b77aacf086ac5010891d
François Joubert <fxa.joubert@gmail.com>
2025-01-13 10:53:22 +0100

    Add day 1 part 2.

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 
 
 fn main() -> anyhow::Result<()> {
@@ -18,10 +20,26 @@ fn main() -> anyhow::Result<()> {
 
     let result: u32 = left
       .iter()
-      .zip(right)
-      .map(|(a,b)| a.abs_diff(b))
+      .zip(&right)
+      .map(|(a,b)| a.abs_diff(*b))
       .sum();
     
     println!("Sum of distances: {}", result);
+
+    // Part 2 
+
+    let frequencies: HashMap<_,_> = right
+      .chunk_by(|a,b| a == b)
+      .map(|group| (group[0], group.len() as u32))
+      .collect();
+
+    let similarity_score: u32 = left
+      .iter()
+      .filter_map(|a| frequencies.get(a)
+        .map(|freq| a * freq))
+      .sum();
+    println!("Similarity score: {}", similarity_score);
+
     Ok(())
+
 }
